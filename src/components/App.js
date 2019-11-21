bd9a19d7a81dbbd9023d1239396af596e7a13c1a
sathishm2408 <sathish.24081995@gmail.com>
2019-11-21 20:02:20 +0530

    Components Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,9 @@
-import React from 'react';
+import React,{Component} from 'react';
+
+export default class App extends Component{
+    render(){
+        return(
+            <h1>Product Page</h1>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from './components/App'
+import Header from './components/Header';
 import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router history={createBrowserHistory()}>
+    <Switch>
+        <Route exact path='/' render={()=><Header><App /></Header>}></Route>
+        {/* <Route exact path='/product/:id' render={()=><Header><Quote /></Header>}></Route> */}
+    </Switch>
+</Router>,
+document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
